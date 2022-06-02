9d4ab87d5dcf76faa0fd21077bbb2d35a3c2ee79
pedved <somichandriicz@gmail.com>
2022-06-02 19:57:38 +0200

    Добавьте файлы проекта.

---
+++ CSharp_Net-module1_3_1_lab/CatchExceptionClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_3_1_lab
+{
+    class CatchExceptionClass
+    {
+        public void CatchExceptionMethod()
+        {
+            try
+            {
+                MyArray ma = new MyArray();
+
+                // 3) replace second elevent of array by 0
+
+                int[] arr = new int[4] { 1, 4, 8, 5 };
+                arr[1] = 0;
+                ma.Assign(arr, 4);
+
+            }
+           
+                // 8) catch all other exceptions here
+            catch (Exception ex)
+            {
+                // 9) print System.Exception properties:
+                // HelpLink, Message, Source, StackTrace, TargetSite
+                Console.WriteLine(ex.HelpLink);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Source);
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.TargetSite);
+            }
+
+            // 10) add finally block, print some message
+            // explain features of block finally
+            finally
+            {
+                Console.WriteLine("Block finaly works");
+            }
+        }
+    }
+}
---
+++ CSharp_Net-module1_3_1_lab/MyArray.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_3_1_lab
+{
+    class MyArray
+    {
+        int[] arr;
+
+        public void Assign(int []arr, int size)
+        {
+            // 5) add block try (outside of existing block try)
+            try
+            {
+                try
+                {
+                    Console.WriteLine(this.arr[10001225451215484854]);
+                    this.arr = new int[size];
+
+                    // 1) assign some value to cell of array arr, which index is out of range
+                    this.arr[this.arr.Length] = 100;
+
+                    for (int i = 0; i < arr.Length; i++)
+                        this.arr[i] = arr[i] / arr[i + 1];
+                    // 7) use unchecked to assign result of operation 1000000000 * 100 
+                    // to last cell of array
+                    unchecked
+                    {
+                        arr[arr.Length - 1] = 1000000000 * 100;
+                    }
+
+                    //NullReferenceException
+
+                }
+                // 2) catch exception index out of rage
+                catch (IndexOutOfRangeException ex)
+                {
+                    // output message 
+                    Console.WriteLine("wrong index");
+                    Console.WriteLine(ex.Message);
+                }
+                catch (DivideByZeroException ex)
+                {
+                    // 4) catch devision by 0 exception
+                    //catch
+                    Console.WriteLine("u tried divide valoe by 0");
+                     Console.WriteLine(ex.Message);
+                }
+            }
+
+            catch (NullReferenceException ex)
+            // 6) add catch block for null reference exception of outside block try  
+
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            // change the code to execute this block (any method of any class)
+            //catch 
+           
+        }
+    }
+}
